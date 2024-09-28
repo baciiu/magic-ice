77c283c7736b6848cd91d7720a7c664acd6e2d75
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2024-09-28 15:42:02 +0200

    reformat with prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,31 @@
-import {Component} from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {NavComponent} from "./layout/nav/nav.component";
-import {HeaderComponent} from "./layout/header/header.component";
-import {StandardComponent} from "./pages/ice/standard/standard.component";
-import {FooterComponent} from "./layout/footer/footer.component";
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './layout/nav/nav.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { StandardComponent } from './pages/ice/standard/standard.component';
+import { FooterComponent } from './layout/footer/footer.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, TranslateModule, NavComponent, HeaderComponent, StandardComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    NavComponent,
+    HeaderComponent,
+    StandardComponent,
+    FooterComponent,
+    HomeComponent,
+  ],
   templateUrl: './app.component.html',
   standalone: true,
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
-    }
+  constructor(private translate: TranslateService) {}
 
   title = 'magic-ice';
-  cards = [1,1,1,1,1,1,1,1];
+  cards = [1, 1, 1, 1, 1, 1, 1, 1];
 }
--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,17 +1,17 @@
-import {Component} from '@angular/core';
-import {TranslateModule, TranslateService} from "@ngx-translate/core";
-import {AvatarModule} from "primeng/avatar";
-import {ToolbarModule} from "primeng/toolbar";
-import {NgClass, NgIf, NgOptimizedImage, NgStyle} from "@angular/common";
-import {Button} from "primeng/button";
-import {InputTextModule} from "primeng/inputtext";
-import {ListboxModule} from "primeng/listbox";
-import {MultiSelectModule} from "primeng/multiselect";
-import {TabViewModule} from "primeng/tabview";
-import {MegaMenuModule} from "primeng/megamenu";
-import {FormsModule} from "@angular/forms";
-import {SplitButtonModule} from "primeng/splitbutton";
-import {SelectButtonModule} from "primeng/selectbutton";
+import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { AvatarModule } from 'primeng/avatar';
+import { ToolbarModule } from 'primeng/toolbar';
+import { NgClass, NgIf, NgOptimizedImage, NgStyle } from '@angular/common';
+import { Button } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ListboxModule } from 'primeng/listbox';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { TabViewModule } from 'primeng/tabview';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { FormsModule } from '@angular/forms';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @Component({
   selector: 'app-nav',
@@ -33,20 +33,20 @@ import {SelectButtonModule} from "primeng/selectbutton";
     ListboxModule,
     FormsModule,
     SplitButtonModule,
-    SelectButtonModule
+    SelectButtonModule,
   ],
-  standalone: true
+  standalone: true,
 })
-
 export class NavComponent {
-
   language = 'ro';
-  languages: any[] = [{ label: 'EN', value: 'en' },{ label: 'RO', value: 'ro' }];
+  languages: any[] = [
+    { label: 'EN', value: 'en' },
+    { label: 'RO', value: 'ro' },
+  ];
   value!: string;
 
-
   constructor(private translate: TranslateService) {
-    translate.addLangs(this.languages.map(option => option.value));
+    translate.addLangs(this.languages.map((option) => option.value));
     translate.setDefaultLang(this.language);
   }
 
